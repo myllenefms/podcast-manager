ac1bd13609fca37f6deddfe17af74e0ec799b37d
Myllene Mendes <mfrsilva@mapfre.com.br>
2025-05-30 16:44:18 -0300

    refatorando responsabilidades das camadas

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -10,7 +10,7 @@ export const getListEpisodes = async (request: IncomingMessage, response: Server
 
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
     
-    const content = await serviceFilterEpisodes(request.url?.split("?")[1].toString().split("=")[1] ?? "");
+    const content = await serviceFilterEpisodes(request);
     response.writeHead(200, {'content-type':"application/json"});
     response.end(JSON.stringify(content));
 }
--- src/services/filter-episode-service.ts
+++ src/services/filter-episode-service.ts
@@ -1,7 +1,13 @@
+import { IncomingMessage } from "http";
 import { repoPodcast } from "../repositories/podcast-repository"
 
 
-export const serviceFilterEpisodes = async (podcastName: string) => {
+export const serviceFilterEpisodes = async (request: IncomingMessage) => {
+
+    const queryString = request.url?.split("?")[1];
+    const value = queryString?.split("=")[1];
+    const podcastName = value ?? "";
+
     const data = await repoPodcast(podcastName);
     return data;
 }
