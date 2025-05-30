c4f9e6710f55ae91f730386e98418fcca286029c
Myllene Mendes <mfrsilva@mapfre.com.br>
2025-05-30 17:22:10 -0300

    eliminação de numeros mágicos e textos flutuantes

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,16 +1,17 @@
 import {IncomingMessage, ServerResponse} from "http";
 import { serviceListEpisodes } from "../services/list-episode-service";
 import { serviceFilterEpisodes } from "../services/filter-episode-service";
+import { StatusCode } from "../utils/status-code";
 
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
     const content = await serviceListEpisodes(); 
-    response.writeHead(200, {'content-type':"application/json"});
+    response.writeHead(StatusCode.OK, {'content-type':"application/json"});
     response.end(JSON.stringify(content))
 }
 
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
     
     const content = await serviceFilterEpisodes(request);
-    response.writeHead(200, {'content-type':"application/json"});
+    response.writeHead(StatusCode.OK, {'content-type':"application/json"});
     response.end(JSON.stringify(content));
 }
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import * as http from "http";
 import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
+import { Routes } from "./routes/routes";
+import { HttpMethod } from "./utils/http-methods";
 
 const server = http.createServer(
     async (request: http.IncomingMessage, response: http.ServerResponse) => {
         
         
-        if(request.method === "GET" && request.url?.split("?")[0] === "/api/list")
+        if(request.method === HttpMethod.GET && request.url?.split("?")[0] === Routes.LIST)
         {
             await getListEpisodes(request, response);
         }
 
-        if (request.method === "GET" && request.url?.split("?")[0] === "/api/episode")
+        if (request.method === HttpMethod.GET && request.url?.split("?")[0] === Routes.EPISODE)
         {
             await getFilterEpisodes(request, response);
         }
